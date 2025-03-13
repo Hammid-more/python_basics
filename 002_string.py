3c13b7305d28b4df505ce788dc16f4d66805cbc9
Hammid-more <lawhammed45@gmail.com>
2025-03-13 09:52:53 +0100

    002_string.py

--- 002_string.py
+++ 002_string.py
@@ -1,8 +1,34 @@
 
 my_sentence = "Python is fun and powerful"
 
-new_message = "Learning python is exicting"
+print(len(my_sentence))
+
+print(my_sentence[0])
+
+print(my_sentence[10:26])
+
+print(my_sentence.lower())
+
+print(my_sentence.count('o'))
+
+print(my_sentence.find('fun'))
+
+my_sentence.replace('powerful' , 'amazing')
+print(my_sentence)
+
+new_message = "Learning python is exicting!"
+
+sentence = my_sentence + new_message
+print(sentence)
+
+sentence = my_sentence + ', ' + new_message
+print(sentence)
 
 sentence = f'{my_sentence}  {new_message}'
+print(sentence)
+
+print(dir(sentence))
 
 print(help(str.replace))
+
+
