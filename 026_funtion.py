1493afd959c94dbabeb129e666a792578b8e519b
Hammid-more <lawhammed45@gmail.com>
2025-04-03 21:05:42 +0100

    026-funtion.py

---
+++ 026_funtion.py
@@ -0,0 +1,19 @@
+def greet_user():
+    print("Hello, welcome to python programing! ")
+greet_user()
+
+
+def add_numbers(a, b):
+    d = a + b
+
+    print(d)
+
+    add_numbers(2, 4)
+
+def mutiply_numbers(a, b):
+    c = a + b
+    return c
+
+result = mutiply_numbers(3, 5)
+
+print(result)
