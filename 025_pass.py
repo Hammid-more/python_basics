a7be0eab440127b96034e6cba9be619e1901c219
Hammid-more <lawhammed45@gmail.com>
2025-04-03 09:34:40 +0100

    025_break.py

---
+++ 025_pass.py
@@ -0,0 +1,14 @@
+
+list_of_names = ["Obasanjo" ,
+                 "Yaradua" ,
+                 "Jonathan" ,
+                 "Buhari" ,
+                 "Tinubu" ] 
+
+for name in list_of_names:
+    
+    if name == "Yaradua":
+
+        pass
+
+    print(name)
