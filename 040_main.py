6ec5320b53dd80adccce3eb29d8a0f832a35a7ae
Hammid-more <lawhammed45@gmail.com>
2025-04-16 22:20:41 +0100

    040_main.py

---
+++ 040_main.py
@@ -0,0 +1,11 @@
+import logic_039
+
+print("Uppercase:", logic_039.to_uppercase("Hello World"))
+
+print("Today's Date:", logic_039.get_today_date())
+
+print("Number of vowels:",logic_039.count_vowels("Learning Python is awesome"))
+
+print("Random number:", logic_039.get_random_number())
+
+print("Full Name:", logic_039.get_full_name())
